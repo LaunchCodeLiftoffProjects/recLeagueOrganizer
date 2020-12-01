1bfccfc5348885380376160928da87cd1e6ff9f5
Doug F <forddf12@outlook.com>
2020-11-30 20:43:16 -0600

    Server Submit

---
+++ server/src/main/java/com/launchcode/RecLeagueOrganizer/RecLeagueOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.RecLeagueOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecLeagueOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecLeagueOrganizerApplication.class, args);
+	}
+
+}
---
+++ server/src/main/java/com/launchcode/RecLeagueOrganizer/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.launchcode.RecLeagueOrganizer.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping
+    @ResponseBody
+    public String hello() {
+        return "Hello, World!";
+        }
+
+}
