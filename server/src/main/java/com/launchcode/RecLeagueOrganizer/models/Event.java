c9db2309cd639d9bfb6ed4092cf71609d9e8adff
bennitalg <40967401+bennitalg@users.noreply.github.com>
2020-12-10 19:06:36 -0600

    Added name to event class

--- server/src/main/java/com/launchcode/RecLeagueOrganizer/models/Event.java
+++ server/src/main/java/com/launchcode/RecLeagueOrganizer/models/Event.java
@@ -10,6 +10,7 @@ public class Event {
     @GeneratedValue
     private int id;
 
+    private String name;
     private String location;
     private String date;
     private String time;
@@ -34,6 +35,18 @@ public class Event {
         this.isEquipmentRequired = isEquipmentRequired;
         this.ageLevel = ageLevel;
         this.skillLevel = skillLevel;
+    }
+
+    public Event(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getLocation() {
