35784b0c0c2eb3fe7e382fb1f72e327a2c943a3b
Don <dbrandon0213@gmail.com>
2021-01-31 15:44:27 -0600

    clear

--- server/src/main/java/com/launchcode/RecLeagueOrganizer/controllers/SearchController.java
+++ server/src/main/java/com/launchcode/RecLeagueOrganizer/controllers/SearchController.java
@@ -18,19 +18,19 @@ public class SearchController {
     @Autowired
     EventRepository eventRepository;
 
-    @PostMapping("/events")
-    public List<Event> searchEvents(@RequestBody Map<String, String> searchTerm) {
-        System.out.println(searchTerm.get("searchTerm"));
+    @GetMapping("/events")
+    public List<Event> searchEvents(@RequestBody String searchTerm) {
+        System.out.println(searchTerm);
         List<Event> searchResults = new ArrayList<Event>();
         for(Event event: eventRepository.findAll()) {
-            if(event.getName().contains(searchTerm.get("searchTerm")) ||
-                    event.getSkillLevel().contains(searchTerm.get("searchTerm")) ||
-                    event.getLocation().contains(searchTerm.get("searchTerm")) ||
-                    event.getEventType().contains(searchTerm.get("searchTerm")) ||
-                    searchTerm.get("searchTerm").contains(event.getName()) ||
-                    searchTerm.get("searchTerm").contains(event.getSkillLevel()) ||
-                    searchTerm.get("searchTerm").contains(event.getEventType()) ||
-                    searchTerm.get("searchTerm").contains(event.getLocation())){
+            if(event.getName().contains(searchTerm) ||
+                    event.getSkillLevel().contains(searchTerm) ||
+                    event.getLocation().contains(searchTerm) ||
+                    event.getEventType().contains(searchTerm) ||
+                    searchTerm.contains(event.getName()) ||
+                    searchTerm.contains(event.getSkillLevel()) ||
+                    searchTerm.contains(event.getEventType()) ||
+                    searchTerm.contains(event.getLocation())){
                 searchResults.add(event);
             }
         }
